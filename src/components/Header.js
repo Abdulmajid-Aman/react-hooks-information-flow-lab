35ae085af5d640c626310dc383d1340979c1396b
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-03 09:29:41 +0300

    Lab

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Head = ({isDarkMode, onDarkModeClick}) => {
+  return (
+    <header>
+    <h2>Shopster</h2>
+    <button onClick={onDarkModeClick}>
+      {isDarkMode ? "Dark" : "Light"} Mode
+    </button>
+  </header>
+  )
+}
+
+export default Head
